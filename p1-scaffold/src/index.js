1a86a167a2ab9a28c96e9dcc5ba6d5bd88457fda
JiaqiGao <jiaqigao12@gmail.com>
2019-05-04 22:11:09 -0500

    Create horizontal bar chart

--- p1-scaffold/src/index.js
+++ p1-scaffold/src/index.js
@@ -1,31 +1,67 @@
-// if the data you are going to import is small, then you can import it using es6 import
-// import MY_DATA from './app/data/example.json'
-// (I tend to think it's best to use screaming snake case for imported json)
 const domReady = require('domready');
 import {prepareData} from './prepare-data';
+import {select} from 'd3-selection';
+import {scaleLinear, scaleBand} from 'd3-scale';
+import {axisBottom, axisLeft} from 'd3-axis';
+// import {max} from 'd3-array';
 
 domReady(() => {
   myVis();
-
 });
 
 function myVis() {
-  // The posters will all be 24 inches by 36 inches
-  // Your graphic can either be portrait or landscape, up to you
-  // the important thing is to make sure the aspect ratio is correct.
+  // portrait view
+  const margin = {
+    top: 15,
+    right: 125,
+    bottom: 115,
+    left: 360
+  };
 
-  // portrait
-  const width = 5000;
-  const height = 36 / 24 * width;
+  // const w = 5000;
+  // const h = (36 / 24 * w);
 
-  // landscape
-  // const height = 5000;
-  // const width = 36 / 24 * height;
-  // console.log('hi!');
+  const w = 1260;
+  const h = 2000;
 
-  // EXAMPLE FIRST FUNCTION
+  const width  = w - margin.left - margin.right;
+  const height = h - margin.top - margin.bottom;
+
+  const svg = select('body').append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${ margin.left },${ margin.top })`);
+
   prepareData('./data/songs.csv').then(data => {
-    console.log('Parsed Data: ', data);
+    const songs = data.songs;
+
+    const x = scaleLinear()
+      .domain([0, 1])
+      .range([0, width]);
+
+    const songNames = scaleBand()
+      .domain(songs.map(d => d.name))
+      .range([0, height])
+      .padding(0.1);
+
+    // append the rectangles for the bar chart
+    svg.selectAll('.bar')
+          .data(songs)
+          .enter().append('rect')
+          .attr('class', 'bar')
+          .attr('width', x(1))
+          .attr('y', d => songNames(d.name))
+          .attr('height', songNames.bandwidth());
+
+    // add the x Axis
+    svg.append('g')
+          .attr('transform', `translate(0,${ height })`)
+          .call(axisBottom(x));
+
+    // add the songNames Axis
+    svg.append('g')
+          .call(axisLeft(songNames));
   });
 
 }
